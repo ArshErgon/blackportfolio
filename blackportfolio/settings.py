b6ba7d820c7cafbbd38af0547f5a503fb246a479
Arsh Ergon <arshergon@gmail.com>
2020-03-03 13:07:27 +0530

    first commit

--- blackportfolio/settings.py
+++ blackportfolio/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Turn it false in Developement
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = False
 
 
 # Application definition
